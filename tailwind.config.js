9800e9300fa671baff706e0d5e65f7de2ac8c405
robiuzzaman4 <robiuzzaman4@gmail.com>
2023-01-20 21:42:33 +0600

    hero done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: "jit",
+  content: ["index.html" , "input.css"],
+  theme: {
+    fontFamily: {
+      "inter": ["Inter", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
